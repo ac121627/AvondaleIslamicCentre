4b83f8c2f10375aaba3ff078d0c34df2fc588344
Faiaz Rahman <ac121627@avcol.school.nz>
2025-07-19 22:24:23 +1200

    Made my Donation link work

---
+++ AvondaleIslamicCentre/Migrations/20250719101022_donationn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvondaleIslamicCentre.Migrations
+{
+    /// <inheritdoc />
+    public partial class donationn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donations",
+                columns: table => new
+                {
+                    DonationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DateDonated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DonorName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    DonationType = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PaymentMethod = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AICUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donations", x => x.DonationId);
+                    table.ForeignKey(
+                        name: "FK_Donations_AspNetUsers_AICUserId",
+                        column: x => x.AICUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donations_AICUserId",
+                table: "Donations",
+                column: "AICUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Donations");
+        }
+    }
+}
